02be2facd1c333ff3a5dc35870b2c1632f4588dd
Peter Rogov <peterrogov@gmail.com>
2023-07-14 22:38:14 +0300

    improve sonarqube workflow

---
+++ .github/workflows/build-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e  # Fail the script if any command fails
+
+projects=(
+    "libraries/lib-boilerplate"
+)
+
+root_directory=$PWD  # Store the absolute path to your repository
+
+for project in "${projects[@]}"; do
+  echo "Building $project..."
+  cd $root_directory/$project
+  npm ci
+  npm run build:release
+  cd $root_directory
+done
