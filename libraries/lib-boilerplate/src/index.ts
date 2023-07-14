a957c14e13ecef84569285b1b41983e00565f673
Peter Rogov <peterrogov@gmail.com>
2023-07-14 17:21:26 +0300

    fix bugs

--- libraries/lib-boilerplate/src/index.ts
+++ libraries/lib-boilerplate/src/index.ts
@@ -2,9 +2,9 @@
  * Humanitech Supply Trail
  *
  * Copyright (c) Humanitech, Peter Rogov and Contributors
- * 
+ *
  * Website: https://humanitech.net
- * Repository: https://github.com/humanitech-net/supply-trail 
+ * Repository: https://github.com/humanitech-net/supply-trail
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -15,6 +15,5 @@
  * @returns 0
  */
 export function dummy() {
-    const t = 12 / 45;
-    return t ? 0 : 0;
+  return 0;
 }
