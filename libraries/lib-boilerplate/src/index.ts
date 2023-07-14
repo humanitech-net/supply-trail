d05f1d7c90cadb26f116bb4a9772479ae0ff1d31
Peter Rogov <peterrogov@gmail.com>
2023-07-14 16:05:09 +0300

    update boilerplate

--- libraries/lib-boilerplate/src/index.ts
+++ libraries/lib-boilerplate/src/index.ts
@@ -1,3 +1,19 @@
+/**
+ * Humanitech Supply Trail
+ *
+ * Copyright (c) Humanitech, Peter Rogov and Contributors
+ * 
+ * Website: https://humanitech.net
+ * Repository: https://github.com/humanitech-net/supply-trail 
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * A dummy function to fill in the space. It does not do anything.
+ * @returns 0
+ */
 export function dummy() {
   return 0;
 }
