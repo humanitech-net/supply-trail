00e10e20fff518c0ee8cb684498e813988793cd2
Peter Rogov <peterrogov@gmail.com>
2023-07-15 12:49:14 +0300

    lower thresholds

--- libraries/lib-boilerplate/jest.config.js
+++ libraries/lib-boilerplate/jest.config.js
@@ -9,10 +9,10 @@ module.exports = {
     coverageReporters: ['text', 'lcov', 'json', 'clover'],
     coverageThreshold: {
         global: {
-            branches: 30,
-            functions: 30,
-            lines: 30,
-            statements: 30
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0
         },
     },
 };
