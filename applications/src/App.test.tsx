c8d9e29d9f2d0af0f33a8edccc8e932a6c54f0ea
phylipos1234 <phylipostesfaye105@gmail.com>
2023-07-25 16:15:33 +0300

    fix sonarcube issue

--- applications/src/App.test.tsx
+++ applications/src/App.test.tsx
@@ -11,7 +11,7 @@
  */
 
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
