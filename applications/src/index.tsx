44a92664a3503d43dc77551916af8c7496a6acd6
phylipos1234 <phylipostesfaye105@gmail.com>
2023-07-25 17:05:19 +0300

    frontend boilerplate

---
+++ applications/src/index.tsx
@@ -0,0 +1,31 @@
+/**
+ * Humanitech Supply Trail
+ *
+ * Copyright (c) Humanitech, Peter Rogov and Contributors
+ *
+ * Website: https://humanitech.net
+ * Repository: https://github.com/humanitech-net/supply-trail
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- build-all.sh
+++ build-all.sh
@@ -5,6 +5,7 @@ set -e  # Fail the script if any command fails
 projects=(
     "libraries/lib-boilerplate"
     "services/service-core"
+    "application"
 )
 
 root_directory=$PWD  # Store the absolute path to your repository
