7aec45d1339f8f17fd4db52b7b1243ab910b3857
Yonas <yonasseyoum24@gmail.com>
2023-07-27 13:51:55 +0300

    lint: fix format

--- services/service-core/test/app.e2e.spec.ts
+++ services/service-core/test/app.e2e.spec.ts
@@ -20,7 +20,7 @@ describe('AppController (e2e)', () => {
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule]
     }).compile();
 
     app = moduleFixture.createNestApplication();
