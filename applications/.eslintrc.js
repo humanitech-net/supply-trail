9d5cbf3d25c9815e277adb815daa0bb8dd086b21
Teddy Abebe <teddyab729@gmail.com>
2023-07-28 16:29:07 -1100

    frontend setup

---
+++ applications/.eslintrc.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // Add any specific ESLint rules you want to enforce
+  },
+};
---
+++ applications/jest.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
