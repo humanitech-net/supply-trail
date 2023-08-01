6f38ffbee50830312fcc4a2ac1bf500cc7677dc2
Yonas <yonasseyoum24@gmail.com>
2023-08-01 14:57:55 +0300

    update config

--- services/service-core/src/app.module.ts
+++ services/service-core/src/app.module.ts
@@ -14,10 +14,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import {configTypeORM} from '../../../config/config-typeorm'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configTypeORM)],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '1234',
+    database: 'humanitech: supply-trail',
+    entities: []
+  })],
   controllers: [AppController],
   providers: [AppService]
 })
