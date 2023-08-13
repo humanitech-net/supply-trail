898e93a65d3ed533ef89ee5565eb02ba181d332d
Peter Rogov <peterrogov@gmail.com>
2023-08-13 15:43:51 +0300

    deploy

---
+++ devops/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ./services/service-core/
+npm ci --ignore-script --production
--- devops/package.sh
+++ devops/package.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mkdir -p package/services/service-core
+mkdir -p ./devops/package/services/service-core
 
-cp -R services/service-core/dist package/services/service-core
-cp services/service-core/package.json package/services/service-core/package.json
-cp services/service-core/package-lock.json package/services/service-core/package-lock.json
+cp -R services/service-core/dist ./devops/package/services/service-core
+cp services/service-core/package.json ./devops/ackage/services/service-core/package.json
+cp services/service-core/package-lock.json ./devops/package/services/service-core/package-lock.json
+
+cp ./devops/deploy.sh ./devops/package/deploy.sh
 
 tar -czvf ./devops/package.tar.gz -C package .
 
