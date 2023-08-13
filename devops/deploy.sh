90ce14115bf01d525e10d1f890c6b0b2013a6939
Peter Rogov <peterrogov@gmail.com>
2023-08-13 21:54:23 +0300

    add some comments

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+# Build service-core container
+
 cd ./services/service-core/
 docker build -t humanitech-supply-trail-service-core:development .
 
+# Reload infrastructure
 cd ../..
 docker-compose -f infrastructure-dev.yml down
 docker-compose -f infrastructure-dev.yml up -d
+
+# Delete package as it is not needed anymore
 rm package.tar.gz
--- devops/package.sh
+++ devops/package.sh
@@ -13,17 +13,20 @@ if [ -z "${HTTPS_CERT_EMAIL}" ]; then
     exit 1
 fi
 
+# ADD services/service-core
 mkdir -p ./devops/package/services/service-core
-
 cp -R services/service-core/dist ./devops/package/services/service-core
 cp services/service-core/package.json ./devops/package/services/service-core/package.json
 cp services/service-core/package-lock.json ./devops/package/services/service-core/package-lock.json
 cp services/service-core/Dockerfile ./devops/package/services/service-core/Dockerfile
 
+# ADD deployment script
 cp ./devops/deploy.sh ./devops/package/deploy.sh
 
+# ADD docker-compose file with injected variables
 cp ./devops/infrastructure-dev.yml ./devops/package/infrastructure-dev.yml
 sed -i "s/__DEPLOYMENT_HOST__/$DEPLOYMENT_HOST/g" ./devops/package/infrastructure-dev.yml
 sed -i "s/__SSL_CERT_EMAIL__/$HTTPS_CERT_EMAIL/g" ./devops/package/infrastructure-dev.yml
 
+# TAR all files together
 tar -czvf ./devops/package.tar.gz -C ./devops/package .
