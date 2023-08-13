a5399b9d0ebc2380c6d3e1c63bb5544ad93d5cb1
Peter Rogov <peterrogov@gmail.com>
2023-08-13 17:18:44 +0300

    update scripts

--- devops/package.sh
+++ devops/package.sh
@@ -9,6 +9,6 @@ cp services/service-core/package-lock.json ./devops/package/services/service-cor
 cp services/service-core/Dockerfile ./devops/package/services/service-core/Dockerfile
 
 cp ./devops/deploy.sh ./devops/package/deploy.sh
-cp ./devops/infrastructure-dev.yml ./infrastructure-dev.yml
+cp ./devops/infrastructure-dev.yml ./devops/package/infrastructure-dev.yml
 
 tar -czvf ./devops/package.tar.gz -C ./devops/package .
