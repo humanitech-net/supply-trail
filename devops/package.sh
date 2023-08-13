9b8c858e2549078123c2282bc06414a62a87f6ef
Peter Rogov <peterrogov@gmail.com>
2023-08-13 21:25:52 +0300

    update script

--- devops/package.sh
+++ devops/package.sh
@@ -11,7 +11,7 @@ cp services/service-core/Dockerfile ./devops/package/services/service-core/Docke
 cp ./devops/deploy.sh ./devops/package/deploy.sh
 
 cp ./devops/infrastructure-dev.yml ./devops/package/infrastructure-dev.yml
-sed -i 's/__DEPLOYMENT_HOST__/"$DEPLOYMENT_HOST"/g' ./devops/package/infrastructure-dev.yml
-sed -i 's/__SSL_CERT_EMAIL__/"$HTTPS_CERT_EMAIL"/g' ./devops/package/infrastructure-dev.yml
+sed -i "s/__DEPLOYMENT_HOST__/$DEPLOYMENT_HOST/g" ./devops/package/infrastructure-dev.yml
+sed -i "s/__SSL_CERT_EMAIL__/$HTTPS_CERT_EMAIL/g" ./devops/package/infrastructure-dev.yml
 
 tar -czvf ./devops/package.tar.gz -C ./devops/package .
