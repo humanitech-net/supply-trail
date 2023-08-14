54c57d679b8441e6ea6558eda57071b4b2f5ed07
Peter Rogov <peterrogov@gmail.com>
2023-08-14 10:08:46 +0300

    fix folder structure

--- build-all.sh
+++ build-all.sh
@@ -5,7 +5,7 @@ set -e  # Fail the script if any command fails
 projects=(
     "libraries/lib-boilerplate"
     "services/service-core"
-    "applications"
+    "applications/webapp"
 )
 
 root_directory=$PWD  # Store the absolute path to your repository
