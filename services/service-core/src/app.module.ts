fad97e9720eb05cbb5e79f55b5bf05e7285acc6b
Yonas <yonasseyoum24gmail.com>
2023-08-15 13:36:37 +0300

    update configuration and add user module

--- services/service-core/src/app.module.ts
+++ services/service-core/src/app.module.ts
@@ -13,29 +13,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
+import { UsersModule } from './database/users/users.module';
+import { TypeormConfig } from './typeorm/typeormconfig';
 
 @Module({
-
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule.forRoot({
-        isGlobal: true,
-        envFilePath: ".local.env"
-      })],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: Number(configService.get('DB_PORT')),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-      }),
-      inject: [ConfigService]
-    })],
+  imports: [TypeormConfig, UsersModule],
   controllers: [AppController],
   providers: [AppService]
 })
-export class AppModule { }
+export class AppModule {}
