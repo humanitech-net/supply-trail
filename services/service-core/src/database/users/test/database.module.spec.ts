d4bcf9a597b017b247386a6ece462f61d97ec5ec
Yonas <yonasseyoum24gmail.com>
2023-08-18 18:52:11 +0300

    add header

--- services/service-core/src/database/users/test/database.module.spec.ts
+++ services/service-core/src/database/users/test/database.module.spec.ts
@@ -1,3 +1,14 @@
+/**
+ * Humanitech Supply Trail
+ *
+ * Copyright (c) Humanitech, Peter Rogov and Contributors
+ *
+ * Website: https://humanitech.net
+ * Repository: https://github.com/humanitech-net/supply-trail
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from '../user.controller';
 import { UsersService } from '../users.service';
