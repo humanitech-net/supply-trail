81dba485ba0e8b9dca2eb6977b59a0f783348d79
Yonas Seyoum <102540240+yonas-seyoum@users.noreply.github.com>
2023-08-22 02:39:56 +0300

    Remove unnecessary code from unit test

--- applications/webapp/src/App.test.tsx
+++ applications/webapp/src/App.test.tsx
@@ -11,11 +11,9 @@
  */
 
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
