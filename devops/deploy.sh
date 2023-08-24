4f1dad9beab23d5935c4b0f30199d69a99685759
Peter Rogov <peterrogov@gmail.com>
2023-08-24 19:34:15 +0300

    use local env file

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -11,8 +11,8 @@ docker build -t humanitech-supply-trail-webapp:development .
 
 # Reload infrastructure
 cd ../..
-docker-compose -f infrastructure-dev.yml down
-docker-compose -f infrastructure-dev.yml up -d
+docker-compose -f infrastructure-dev.yml --enf-file=.dev.env down
+docker-compose -f infrastructure-dev.yml --enf-file=.dev.env up -d
 
 # Delete package as it is not needed anymore
 rm package.tar.gz
