c696506df93f608c314d79e8b0d5a5bb0822dc8c
leulseged gebremedhin <leulsegedgebremedhin@gmail.com>
2023-08-30 16:48:15 +0300

    Merge branch 'fix-borken-dependencies' into local-running-branch

--- build-all.sh
+++ build-all.sh
@@ -9,11 +9,11 @@ projects=(
 )
 
 root_directory=$PWD  # Store the absolute path to your repository
+npm install --ignore-scripts
 
 for project in "${projects[@]}"; do
   echo "Building $project..."
   cd $root_directory/$project
-  npm install --ignore-scripts
   npm run build:release
   cd $root_directory
 done
