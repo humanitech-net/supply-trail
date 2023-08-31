eecd653ff78d67ff8c263c9a71131e323364e975
Yonas <yonasseyoum24@gmail.com>
2023-08-31 19:00:27 +0300

    add unit test for button component

---
+++ applications/webapp/src/Components/Button.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import GraphQlButton, { Connection } from './Button';
+import { MockedProvider } from '@apollo/client/testing';
+import { findByTestId } from '@testing-library/dom';
+
+const mocks = [
+  {
+    request: {
+      query: Connection,
+    },
+    result: {
+      data: {
+        findAll: {
+          id: 1,
+          firstName: 'John',
+        },
+      },
+    },
+  },
+];
+
+describe('GraphQlButton', () => {
+  it('should update the id and name when the button is clicked', async () => {
+    const { getByTestId } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <GraphQlButton />
+      </MockedProvider>
+    );
+
+    const button = getByTestId('button');
+    fireEvent.click(button);
+
+      const idElement = await findByTestId(document.documentElement,'id');
+      const nameElement = await findByTestId(document.documentElement, 'test');
+
+      expect(Number(idElement.textContent)).toBe(1);
+      expect(nameElement.textContent).toBe('John');
+  });
+});
