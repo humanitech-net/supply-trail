45ce195577ca6c05faa33a84073be28ea25108e5
Yonas <yonasseyoum24@gmail.com>
2023-08-31 19:57:23 +0300

    remove unused var

--- applications/webapp/src/Components/Button.test.tsx
+++ applications/webapp/src/Components/Button.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render, fireEvent} from '@testing-library/react';
-import GraphQlButton, { Connection } from './Button';
-import { MockedProvider } from '@apollo/client/testing';
-import { findByTestId } from '@testing-library/dom';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import GraphQlButton, { Connection } from "./Button";
+import { MockedProvider } from "@apollo/client/testing";
+import { findByTestId } from "@testing-library/dom";
 
 const mocks = [
   {
@@ -13,28 +13,28 @@ const mocks = [
       data: {
         findAll: {
           id: 1,
-          firstName: 'John',
+          firstName: "John",
         },
       },
     },
   },
 ];
 
-describe('GraphQlButton', () => {
-  it('should update the id and name when the button is clicked', async () => {
+describe("GraphQlButton", () => {
+  it("should update the id and name when the button is clicked", async () => {
     const { getByTestId } = render(
       <MockedProvider mocks={mocks} addTypename={false}>
         <GraphQlButton />
-      </MockedProvider>
+      </MockedProvider>,
     );
 
-    const button = getByTestId('button');
+    const button = getByTestId("button");
     fireEvent.click(button);
 
-      const idElement = await findByTestId(document.documentElement,'id');
-      const nameElement = await findByTestId(document.documentElement, 'test');
+    const idElement = await findByTestId(document.documentElement, "id");
+    const nameElement = await findByTestId(document.documentElement, "test");
 
-      expect(Number(idElement.textContent)).toBe(1);
-      expect(nameElement.textContent).toBe('John');
+    expect(Number(idElement.textContent)).toBe(1);
+    expect(nameElement.textContent).toBe("John");
   });
 });
