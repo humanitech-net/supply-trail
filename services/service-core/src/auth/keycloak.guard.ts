bd177c09f4063ffe556d50526c3461335d015f84
Yonas <yonasseyoum24@gmail.com>
2023-09-18 19:14:50 +0300

    add header to keycloak guard and test

--- services/service-core/src/auth/keycloak.guard.ts
+++ services/service-core/src/auth/keycloak.guard.ts
@@ -1,3 +1,15 @@
+/**
+ * Humanitech Supply Trail
+ *
+ * Copyright (c) Humanitech, Peter Rogov and Contributors
+ *
+ * Website: https://humanitech.net
+ * Repository: https://github.com/humanitech-net/supply-trail
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { ConfigService } from '@nestjs/config';
