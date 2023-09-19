584b2e2d1ed67fc969bab4145d87ad524a65af23
Yonas <yonasseyoum24@gmail.com>
2023-09-19 17:41:40 +0300

    update: login-required to access webpage and add logout functionality

--- applications/webapp/src/Authentication/Keycloak.ts
+++ applications/webapp/src/Authentication/Keycloak.ts
@@ -1,3 +1,15 @@
+/**
+ * Humanitech Supply Trail
+ *
+ * Copyright (c) Humanitech, Peter Rogov and Contributors
+ *
+ * Website: https://humanitech.net
+ * Repository: https://github.com/humanitech-net/supply-trail
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
@@ -10,7 +22,7 @@ export const initKeycloak = () => {
   return new Promise<void>((resolve, reject) => {
     keycloak
       .init({
-        onLoad: "check-sso",
+        onLoad: "login-required",
       })
       .then((authenticated) => {
         if (authenticated) {
@@ -35,4 +47,9 @@ export const refreshToken = () => {
     .catch((error) => {
       console.log("Failed to refresh token:", error);
     });
+};
+
+export const logout = () => {
+  keycloak.logout();
+  localStorage.removeItem("token");
 };
