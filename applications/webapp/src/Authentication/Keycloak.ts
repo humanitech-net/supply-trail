9a13d59b19709ce776fdd3af6d3a03b79205d142
leulseged gebremedhin <leulsegedgebremedhin@gmail.com>
2023-09-29 14:37:22 +0300

    updated infra-dev yml file

--- applications/webapp/src/Authentication/Keycloak.ts
+++ applications/webapp/src/Authentication/Keycloak.ts
@@ -14,7 +14,7 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
   realm: "humanitech",
-  url: "https://dev-iam.supply-trail.humanitech.net",
+  url: "https://dev.supply-trail.humanitech.net/auth",
   clientId: "supply-trail-app",
 });
 
