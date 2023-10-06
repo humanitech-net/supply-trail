71f07eeb09a265ba8181537e95f4bc7d49fb24b9
Yonas <yonasseyoum24@gmail.com>
2023-10-06 18:26:19 +0300

    add components for the user page

---
+++ applications/webapp/src/Pages/User/components/detailHolder.tsx
@@ -0,0 +1,22 @@
+/**
+ * Humanitech Supply Trail
+ *
+ * Copyright (c) Humanitech, Peter Rogov and Contributors
+ *
+ * Website: https://humanitech.net
+ * Repository: https://github.com/humanitech-net/supply-trail
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from "react";
+import { Card, CardContent } from "@mui/material";
+
+export default function DetailHolder() {
+  return (
+    <Card sx={{ height: "260px" }}>
+      <CardContent>user detail</CardContent>
+    </Card>
+  );
+}
