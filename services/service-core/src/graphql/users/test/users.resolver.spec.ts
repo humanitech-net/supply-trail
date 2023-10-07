a127c3948db1b2da3ba24eaea65efd4074a1bd08
leulseged gebremedhin <leulsegedgebremedhin@gmail.com>
2023-10-07 14:43:20 +0300

    modifying the use resolver test

--- services/service-core/src/graphql/users/test/users.resolver.spec.ts
+++ services/service-core/src/graphql/users/test/users.resolver.spec.ts
@@ -9,23 +9,49 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from '../users.resolver';
+import { UsersService } from '../users.service';
+import { KeycloakService } from '../../../auth/keycloak.service';
 
 describe('UsersResolver', () => {
   let usersResolver: UsersResolver;
+  let keycloakService: KeycloakService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver]
+      providers: [
+        UsersResolver,
+        UsersService,
+        KeycloakService // Mock or provide a stub if necessary
+      ]
     }).compile();
+
     usersResolver = module.get<UsersResolver>(UsersResolver);
+    keycloakService = module.get<KeycloakService>(KeycloakService);
   });
 
-  describe('findAll', () => {
-    it('should return all users', () => {
-      expect(typeof usersResolver.findAll()).toBe('object');
+  it('should be defined', () => {
+    expect(usersResolver).toBeDefined();
+  });
+
+  describe('getUser', () => {
+    it('should return a user', async () => {
+      // Mock the behavior of keycloakService.getUser
+      const mockUser = {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john.doe@example.com',
+        username: 'testuser'
+      };
+      jest.spyOn(keycloakService, 'getUser').mockResolvedValue(mockUser);
+
+      // Call the resolver method
+      const result = await usersResolver.getUser('token');
+
+      // Verify that the result matches the expected user
+      expect(result).toEqual(mockUser);
     });
   });
 });
