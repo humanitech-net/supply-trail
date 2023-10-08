e4ed02f4a606883400d1230dc4d49282ef089471
Yonas <yonasseyoum24@gmail.com>
2023-10-08 22:39:09 +0300

    update detailHolder component

--- applications/webapp/src/Pages/User/components/detailHolder.tsx
+++ applications/webapp/src/Pages/User/components/detailHolder.tsx
@@ -11,12 +11,103 @@
  */
 
 import React from "react";
-import { Card, CardContent } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  FormControl,
+  Grid,
+  InputLabel,
+  OutlinedInput,
+  useTheme,
+} from "@mui/material";
+import { styles } from "../styles/style";
 
-export default function DetailHolder() {
+interface DetailHolderProps {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  birthdate: string;
+}
+
+export default function DetailHolder({
+  firstName,
+  lastName,
+  email,
+  phoneNumber,
+  address,
+  birthdate,
+}: DetailHolderProps) {
+  const theme = useTheme();
+  const style = styles(theme).detailHolder;
   return (
-    <Card sx={{ height: "260px" }}>
-      <CardContent>user detail</CardContent>
+    <Card elevation={0} sx={style.card}>
+      <CardContent>
+        <Grid container spacing={2} marginTop={1} sx={style.grid}>
+          <Grid item xs={6}>
+            <FormControl fullWidth>
+              <InputLabel>First Name</InputLabel>
+              <OutlinedInput
+                defaultValue={firstName}
+                label="First Name"
+                disabled
+              />
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={6}>
+            <FormControl fullWidth>
+              <InputLabel>Last Name</InputLabel>
+              <OutlinedInput
+                defaultValue={lastName}
+                label="Last Name"
+                disabled
+              />
+            </FormControl>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} sx={{ marginBottom: "1rem" }}>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel>Email</InputLabel>
+              <OutlinedInput defaultValue={email} label="Email" disabled />
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <InputLabel>Phone Number</InputLabel>
+              <OutlinedInput
+                defaultValue={phoneNumber}
+                label="Phone Number"
+                disabled
+              />
+            </FormControl>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <FormControl fullWidth>
+              <InputLabel>Address</InputLabel>
+              <OutlinedInput defaultValue={address} label="Address" disabled />
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={6}>
+            <FormControl fullWidth>
+              <InputLabel>Date of Birth</InputLabel>
+              <OutlinedInput
+                defaultValue={birthdate}
+                label="Date of Birth"
+                disabled
+              />
+            </FormControl>
+          </Grid>
+        </Grid>
+      </CardContent>
     </Card>
   );
 }
