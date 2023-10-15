25929a5b34b755e32d07cdea4782311325d915a0
Yonas <yonasseyoum24@gmail.com>
2023-10-15 20:12:25 +0300

    add resolver

--- services/service-core/src/graphql/users/users.resolver.ts
+++ services/service-core/src/graphql/users/users.resolver.ts
@@ -9,10 +9,10 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { Query, Resolver, Context } from '@nestjs/graphql';
-import { Users } from './users.entity';
+import { Query, Resolver, Context, Args, Mutation } from '@nestjs/graphql';
+import { Users, updateUser } from './users.entity';
 import { KeycloakService } from '../../auth/keycloak.service';
-import { Request } from 'express'; // Import Request from express
+import { Request } from 'express';
 
 @Resolver(() => Users)
 export class UsersResolver {
@@ -25,5 +25,15 @@ export class UsersResolver {
     const token = req.headers['authorization'].split(' ')[1];
 
     return this.keycloakService.getUser(token);
+  }
+
+  @Mutation(() => String)
+  async editUser(
+    @Context() context: { req: Request },
+    @Args('userInput') userInput: updateUser
+  ): Promise<string> {
+    const user = await this.getUser(context);
+    const ID = user.id.toString();
+    return await this.keycloakService.editUser(ID, userInput);
   }
 }
