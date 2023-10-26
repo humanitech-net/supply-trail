d348dc45db7a33cec4cd561dbd20b69a8de59238
Yonas <yonasseyoum24@gmail.com>
2023-10-27 02:45:44 +0300

    add context providers

---
+++ applications/webapp/src/Pages/User/components/ContextProvider/UserPageContextProvider.tsx
@@ -0,0 +1,29 @@
+/**
+ * Humanitech Supply Trail
+ *
+ * Copyright (c) Humanitech, Peter Rogov and Contributors
+ *
+ * Website: https://humanitech.net
+ * Repository: https://github.com/humanitech-net/supply-trail
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from "react";
+import { useCurrentUserData } from "src/hooks/useCurrentUserData";
+import { UserContext } from "../../context";
+
+export function UserPageContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { user, loading, error } = useCurrentUserData();
+
+  return (
+    <UserContext.Provider value={{ user, loading, error }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
