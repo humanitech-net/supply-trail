e0270915d23c345b80c5dcfa3c7b67ef259baa5b
Yonas <yonasseyoum24@gmail.com>
2023-10-27 02:50:49 +0300

    add error and loading components

---
+++ applications/webapp/src/Pages/User/components/loadingOverlay.tsx
@@ -0,0 +1,24 @@
+/**
+ * Humanitech Supply Trail
+ *
+ * Copyright (c) Humanitech, Peter Rogov and Contributors
+ *
+ * Website: https://humanitech.net
+ * Repository: https://github.com/humanitech-net/supply-trail
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from "react";
+import { useUserContext } from "../context";
+
+export default function LoadingOverlay({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { loading } = useUserContext();
+
+  return loading ? <>Overlay spinner goes here</> : <>{children}</>;
+}
