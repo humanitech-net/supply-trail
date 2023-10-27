9b56d2271cdec03dd6ecfb769673aa9fd3f03b8e
Yonas <yonasseyoum24@gmail.com>
2023-10-27 17:33:51 +0300

    update userPage unit test

--- applications/webapp/src/Pages/User/test/userPage.test.tsx
+++ applications/webapp/src/Pages/User/test/userPage.test.tsx
@@ -14,12 +14,13 @@ import React from "react";
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import UserPage from "../userPage";
 import { MockedProvider } from "@apollo/client/testing";
-import { GET_USER_QUERY } from "src/hooks/query";
+import { GET_USER_QUERY } from "../../../hooks/query";
 import { BrowserRouter } from "react-router-dom";
 
 describe("UserPage", () => {
   const mockData = {
     getUser: {
+      id: "id",
       username: "username",
       firstName: "firstName",
       lastName: "lastName",
